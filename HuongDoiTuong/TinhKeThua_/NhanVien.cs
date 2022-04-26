ccd1eacea63ed2e28d615a835b5ea3b377c85b5c
LAPTOP-7M6VPU5A\ASUS <ngocyen2812.pham@gmail.com>
2022-04-26 11:15:40 +0700

    add

---
+++ HuongDoiTuong/TinhKeThua_/NhanVien.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TinhKeThua_
+{
+    class NhanVien : Nguoi
+    {
+        private string bangcap;
+        public NhanVien(string maso, string hoten, string gioitinh, string bangcap)
+            : base(maso, hoten, gioitinh)
+        {
+            this.bangcap = bangcap;
+        }
+
+        public override void Nhap()
+        {
+            base.Nhap();
+            Console.Write("Nhap thong tin bang cap: ");
+            bangcap = Console.ReadLine();
+        }
+        public override void HienThi()
+        {
+            base.HienThi();
+            Console.WriteLine("Bang cao: {0}", bangcap);
+        }
+
+        static void Main(string[] args)
+        {
+            NhanVien nv = new NhanVien("", "", "", "");
+            nv.Nhap();
+            nv.HienThi();
+
+        }
+    }
+}
+    //https://www.quantrimaytinh.com/2020/05/tinh-da-hinh-trong-c-sharp.html
